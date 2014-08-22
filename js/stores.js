dfe988414447ee5d2d9b168fc9e02afc603085b4
George Geeslin <george.geeslin@gmail.com>
2014-08-21 22:34:52 -0500

    Initial Commit
    Site as it currently sit. Made it some time ago. Lots of changes and
    updates soon to come.

---
+++ js/stores.js
@@ -0,0 +1,235 @@
+$(document).ready(function(){
+   $("#accordion").accordion();   
+})
+
+$(document).ready(function(){
+	$("#Tri-County_Farm").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Tri-County+Farm+%26+Ranch+Supply&amp;aq=&amp;sll=32.483886,-99.745403&amp;sspn=0.330731,0.196381&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;ll=36.249499,-91.589928&amp;spn=0.011698,0.020921&amp;z=16&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})		
+	$("#Northland_Volkswagen_Cincinnati").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3094.134019901998!2d-84.46574689999997!3d39.14893330000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa0c2dbc06de4b782!2sNorthland+Volkswagen!5e0!3m2!1sen!2sus!4v1395362258026" width="976" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})	
+	$("#Eastern_Arkansas").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Eastern+Arkansas+Industrial+Tractor+1520+W+Broadway+Forrest+City,+AR&amp;hl=en&amp;sll=35.001436,-90.800174&amp;sspn=0.027244,0.055747&amp;hq=Eastern+Arkansas+Industrial+Tractor+1520+W+Broadway&amp;hnear=Forrest+City,+St+Francis,+Arkansas&amp;t=m&amp;ie=UTF8&amp;ll=35.001436,-90.800174&amp;spn=0.014765,0.022748&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})	
+	$("#ACCO_Farm_Ranch").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3367.3415558230736!2d-99.72573344999999!3d32.43679125!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4721511b58cc8fd5!2sA+C+Co+Farm+%26+Ranch+Center!5e0!3m2!1sen!2sus!4v1395363732757" width="979" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})		
+	$("#Archer_Feed").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3323.386809395615!2d-98.62280095000001!3d33.59526685!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcf0ed1f6384594d2!2sArcher+Feed!5e0!3m2!1sen!2sus!4v1395626911198" width="979" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})		
+	$("#EZ_Feed").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=E+Z+Feed+%26+Supply,+9580+U.S.+283,+Albany,+TX&amp;hl=en&amp;sll=32.777293,-99.272095&amp;sspn=0.055928,0.111494&amp;oq=E+Z+Feed+%26+Supply,+9580+U.S.+283,+Albany,+TX&amp;hq=ez+feed+%26+supply&amp;hnear=U.S.+283,+Albany,+Texas+76430&amp;t=m&amp;ie=UTF8&amp;ll=32.777293,-99.272095&amp;spn=0.033021,0.006295&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})			
+	})	
+	$("#Four_Seasons").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Four+Seasons+Fast+Lube+%26+Car,+South+27th+Street,+Abilene,+TX&amp;hl=en&amp;sll=32.428766,-99.729576&amp;sspn=0.112291,0.222988&amp;oq=Four+Seasons+Fast+Lube&amp;hq=Four+Seasons+Fast+Lube+%26+Car,&amp;hnear=S+27th+St,+Abilene,+Texas&amp;t=m&amp;ie=UTF8&amp;ll=32.417976,-99.751263&amp;spn=0.006295,0.019503&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})		
+	$("#Jackson_Bros").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Jackson+Bros+Feed+%26+Seed,+South+Treadaway+Boulevard,+Abilene,+TX&amp;hl=en&amp;sll=32.417976,-99.751263&amp;sspn=0.014038,0.027874&amp;oq=Jackson+Bros+F&amp;hq=Jackson+Bros+Feed+%26+Seed,&amp;hnear=S+Treadaway+Blvd,+Abilene,+Texas+79602&amp;t=m&amp;ie=UTF8&amp;ll=32.406393,-99.743137&amp;spn=0.036023,0.006295&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Lithia_Toyota").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Lithia+Toyota+of+Abilene,+Southwest+Drive,+Abilene,+TX&amp;hl=en&amp;sll=32.406393,-99.743137&amp;sspn=0.05616,0.111494&amp;oq=Lithia_Toyota&amp;hq=Lithia+Toyota+of+Abilene,+Southwest+Drive,+Abilene,+TX&amp;t=m&amp;ie=UTF8&amp;ll=32.407647,-99.781385&amp;spn=0.01884,0.032015&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Templeton_Equipment").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Templeton+Equipment+Co,+South+Treadaway+Boulevard,+Abilene,+TX&amp;hl=en&amp;sll=32.407647,-99.781385&amp;sspn=0.028079,0.055747&amp;oq=Templeton+&amp;hq=Templeton+Equipment+Co,&amp;hnear=S+Treadaway+Blvd,+Abilene,+Texas+79602&amp;t=m&amp;ie=UTF8&amp;ll=32.421853,-99.736518&amp;spn=0.070035,0.012001&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Berend_Bros_Bowie").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3324.837872859033!2d-97.85016329999999!3d33.55758845!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc29b0d461e739e50!2sBerend+Brothers!5e0!3m2!1sen!2sus!4v1395629352105" width="976" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Breckenridge_Auto").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=1811+W+Walker+St,+Breckenridge,+TX&amp;hl=en&amp;sll=32.755338,-98.909424&amp;sspn=0.027971,0.055747&amp;oq=1811+W+Walker+St&amp;hnear=1811+W+Walker+St,+Breckenridge,+Stephens,+Texas&amp;t=m&amp;ie=UTF8&amp;hq=&amp;z=14&amp;ll=32.753336,-98.93034&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Four_H").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Four+H+Feed,+2456+U.S.+180,+Breckenridge,+TX&amp;hl=en&amp;sll=32.748987,-98.9218&amp;sspn=0.027973,0.055747&amp;oq=Four+H+Feed+2456+US+Highway+180+E&amp;hq=four+h+feed&amp;hnear=2456+U.S.+180,+Breckenridge,+Stephens,+Texas&amp;t=m&amp;ie=UTF8&amp;ll=32.756711,-98.934932&amp;spn=0.013985,0.027874&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Warehouse_Feed").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Warehouse+Feed,+County+Road+245,+Breckenridge,+TX&amp;aq=0&amp;oq=Warehouse+Feed&amp;sll=32.482542,-99.985886&amp;sspn=0.330735,0.196381&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;ll=32.757072,-98.869872&amp;spn=0.012199,0.020921&amp;z=16&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Deer_Blind").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=6301+Hwy+206,+Cisco,+TX&amp;hl=en&amp;sll=32.845125,-98.432241&amp;sspn=1.78826,3.56781&amp;oq=6301+Hwy+206&amp;hnear=Texas+206,+Cisco,+Texas+76437&amp;t=m&amp;ie=UTF8&amp;hq=&amp;z=10&amp;ll=32.227905,-99.019775&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Thornton_Feed").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Thornton+Feed+Mill+Inc,+Conrad+Hilton+Boulevard,+Cisco,+TX&amp;hl=en&amp;sll=32.227905,-99.019775&amp;sspn=0.900316,1.783905&amp;oq=Thornton+Feed+Mill&amp;hq=Thornton+Feed+Mill+Inc,&amp;hnear=Conrad+Hilton+Blvd,+Cisco,+Texas+76437&amp;t=m&amp;ie=UTF8&amp;ll=32.384938,-98.977127&amp;spn=0.006295,0.006295&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Lawrence_Farm").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lawrence+Farm+%26+Ranch+Supply+Cross+Plains+TX&amp;aq=&amp;sll=32.253078,-99.069734&amp;sspn=0.450038,0.891953&amp;ie=UTF8&amp;hq=Lawrence+Farm+%26+Ranch+Supply&amp;hnear=Cross+Plains,+Callahan,+Texas&amp;t=m&amp;ll=32.121219,-99.16234&amp;spn=0.011067,0.006605&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Denver_City").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Denver+City+Motor+Parts,+Inc.,+East+Broadway+Street,+Denver+City,+TX&amp;hl=en&amp;sll=32.253078,-99.069734&amp;sspn=0.450038,0.891953&amp;oq=Denver+City+Motor+Parts+Inc&amp;hq=Denver+City+Motor+Parts,+Inc.,&amp;hnear=E+Broadway+St,+Denver+City,+Yoakum,+Texas+79323&amp;t=m&amp;ie=UTF8&amp;ll=32.964324,-102.828258&amp;spn=0.006295,0.009098&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Kens_Motorcycle_Shop").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3368.650273668773!2d-98.7977719!3d32.40173535!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7c20df2931f28fc8!2sKen&#39;s+Motorcycle+Shop+Inc.!5e0!3m2!1sen!2sus!4v1395363062120" width="976" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Holliday_Feed").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3314.7563325880596!2d-98.68692225000001!3d33.8186007!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x126eb55c0e9f8ba0!2sHolliday+Feed+%26+Farm+Supply!5e0!3m2!1sen!2sus!4v1395626996591" width="979" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	
+		$("#Berend_Bros_Olney").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3332.331814292739!2d-98.7649933!3d33.3624023!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7f8fbf7d8944150b!2sBerend+Brothers+Inc!5e0!3m2!1sen!2sus!4v1395629842746" width="976" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	
+	$("#Automotive_Plus").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Automotive+Plus,+230+W+Dallas+St,+Munday,+TX&amp;hl=en&amp;sll=32.60797,-97.322759&amp;sspn=0.112068,0.222988&amp;oq=Automotive+Plus+230+W+Dallas+St&amp;hq=Automotive+Plus,&amp;hnear=W+Dallas+St,+Munday,+Knox,+Texas+76371&amp;t=m&amp;ie=UTF8&amp;ll=33.446114,-99.628786&amp;spn=0.006295,0.006295&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#CJJs").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=604+W+South+1st+St+Roby+TX&amp;aq=&amp;sll=32.74423,-100.381604&amp;sspn=0.006994,0.013937&amp;ie=UTF8&amp;hq=&amp;hnear=604+S+1st+St,+Roby,+Texas+79543&amp;ll=32.744232,-100.381608&amp;spn=0.006994,0.013937&amp;t=m&amp;z=14&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(2500, 1)
+		})		
+	})
+	$("#Ben_Wilson").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Ben+Wilson+Supply,+1507+25th+Street,+Snyder,+TX&amp;hl=en&amp;sll=32.74423,-100.381604&amp;sspn=0.006994,0.013937&amp;oq=Ben+Wilson+Supply+1507+25th+St&amp;hq=Ben+Wilson+Supply,&amp;hnear=1507+25th+St,+Snyder,+Scurry,+Texas&amp;t=m&amp;ie=UTF8&amp;ll=32.717209,-100.914456&amp;spn=0.006295,0.006295&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Snyder_Farm").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Snyder+Farm+%26+Ranch+Supply,+37th+Street,+Snyder,+TX&amp;hl=en&amp;sll=32.70448,-100.905234&amp;sspn=0.006997,0.013937&amp;oq=Snyder+Farm+%26+Ranch+Supply,+&amp;hq=Snyder+Farm+%26+Ranch+Supply,&amp;hnear=37th+St,+Snyder,+Texas+79549&amp;t=m&amp;ie=UTF8&amp;ll=32.70448,-100.905234&amp;spn=0.006295,0.050811&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Throckmorton_AG").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe width="976" height="676" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Throckmorton+AG+Products+Inc&amp;aq=&amp;sll=32.483886,-99.745403&amp;sspn=0.330731,0.196381&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;ll=33.17849,-99.178755&amp;spn=0.012141,0.020921&amp;z=16&amp;output=embed"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	$("#Berend_Bros_Wichita").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3312.8144692988635!2d-98.4943298!3d33.868672000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd501de9214cdb318!2sBerend+Bros!5e0!3m2!1sen!2sus!4v1395628797635" width="979" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})
+	$("#Berend_Bros_Windthorst").click(function(){	
+		$("#preview_map").fadeTo(600, 0, function(){	
+		$("#preview_map").html('<div id="loading_hider"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3325.7179772005993!2d-98.43927095000001!3d33.534717400000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x14c1a77087302031!2sBerend+Brothers+Store!5e0!3m2!1sen!2sus!4v1395628298977" width="979" height="676" frameborder="0" style="border:0"></iframe></div>')
+		$("#loading_hider").css({opacity:'0'})
+		$("#preview_map").fadeTo(1,1)
+		$("#loading_hider").fadeTo(1000, 1)
+		})		
+	})	
+	
+})
+
+
